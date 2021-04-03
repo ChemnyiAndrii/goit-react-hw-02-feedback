791a9ea4dfbb038580e165b2e9f482bdd14c963b
Andrii Chemnyi <65915107+ChemnyiAndrii@users.noreply.github.com>
2021-04-03 23:38:45 +0300

    Завдання

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { Statistics } from "./components/Statistics/Statistics";
+import { FeedbackOptions } from "./components/FeedbackOptions/Feedback";
+import { Section } from "./components/Section/section";
+import { Notification } from "./components/Notification/Notification";
+
+export default class App extends Component {
+  state = { good: 0, neutral: 0, bad: 0 };
+  addFeedback = (e) => {
+    e.persist();
+    this.setState((prevState) => {
+      return { [e.target.name]: prevState[e.target.name] + 1 };
+    });
+  };
+
+  countTotalFeedback() {
+    return Object.values(this.state).reduce((acc, stat) => {
+      acc += stat;
+      return acc;
+    }, 0);
+  }
+
+  countPositiveFeedbackPercentage() {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  }
+
+  render() {
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.addFeedback}
+          />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="No feedback given"></Notification>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
---
+++ src/components/FeedbackOptions/Feedback.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+export const FeedbackOptions = ({ options, onLeaveFeedback }) =>
+  options.map((name) => (
+    <button key={name} name={name} onClick={onLeaveFeedback}>
+      {name}
+    </button>
+  ));
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+    }).isRequired
+  ),
+  onLeaveFeedback: PropTypes.func,
+};
